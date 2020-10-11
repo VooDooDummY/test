988d396acb77bbe22043fdfc89d9cd0dc7caabff
Vova <voodoodummy1992@gmail.com>
2020-10-11 13:34:07 +0300

    test work js-file

--- project/script.js
+++ project/script.js
@@ -9,12 +9,27 @@ const personalMovieDB ={
     privat: false
 };
 
-const aFilm= prompt('Один из последних просмотренных фильмов',''),
-      bRate= prompt('На сколько оцените его?',''),
-      cFilm= prompt('Один из последних просмотренных фильмов',''),
-      dRate= prompt('На сколько оцените его?','');
+for (let i = 0; i < 2; i++) {
+    const aFilm= prompt('Один из последних просмотренных фильмов',''),
+      bRate= prompt('На сколько оцените его?','');
 
-personalMovieDB.movies[aFilm] = bRate;
-personalMovieDB.movies[cFilm] = dRate;
+    if (aFilm != null && bRate != null && aFilm != '' && bRate != '' && aFilm.length < 50){
+        personalMovieDB.movies[aFilm] = bRate;
+        console.log ('done');
+    } else {
+        console.log ('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10){
+    console.log ('Просмотрено мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+ console.log ('Вы классический зритель');
+} else if (personalMovieDB.count >=30) {
+    console.log ('Вы киноман');
+} else {
+    console.log ('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
